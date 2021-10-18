affce0101af8caba0d5e031609c99535e3bcdbff
Pedro Barão <phbarao@gmail.com>
2021-10-18 18:29:20 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import { motion } from "framer-motion";
+
+const cats = [
+  "Dirceu",
+  "Bozó",
+  "Letícia",
+  "Bento",
+  "Gato Amarelo",
+  "Branquinha",
+  "Vaquinha",
+];
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: #101010;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const List = styled.ul`
+  width: 100%;
+  max-width: 800px;
+  color: #464646;
+  list-style: none;
+
+  display: grid;
+  grid-template-columns: 1fr;
+  gap: 10px;
+  padding: 10px;
+`;
+
+const ListItem = styled.li`
+  background: #fff;
+  padding: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 10px;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <List>
+        {cats.map((cat, i) => (
+          <motion.div
+            key={cat}
+            initial={{
+              opacity: 0,
+              translateY: -50,
+            }}
+            animate={{ opacity: 1, translateY: 0 }}
+            transition={{ duration: 1, delay: i * 0.1 }}
+          >
+            <ListItem>{cat}</ListItem>
+          </motion.div>
+        ))}
+      </List>
+    </Container>
   );
 }
 
